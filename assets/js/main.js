3049b3461b906ed56c8b37aeadefafea3b19194b
Alexandre Lefebvre <yeakoo@outlook.com>
2018-05-28 19:41:49 +0000

    Adding streams

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,16 @@
     document.getElementById('search').addEventListener('click', e => {
         let name = document.getElementById('player-search').value;
         if (name.length < 1) return;
-        console.log(name);
+        socket.emit('search', { name: name });
     });
 
     document.getElementById('player-search').addEventListener("keyup", function(event) {
         event.preventDefault();
         if (event.keyCode === 13)
             document.getElementById("search").click();
+    });
+    
+    document.getElementById('next-section').addEventListener('click', e => {
+       window.location.replace('#streams') 
     });
 })();
