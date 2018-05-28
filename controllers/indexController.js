2b50be0131a0ef080f2227459081ec593a6381df
Alexandre Lefebvre <yeakoo@outlook.com>
2018-05-28 20:29:36 +0000

    Adding twitch api

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,25 @@
+const api = require('twitch-api-v5');
+api.clientID = 'vtqebhinodx3rzi7xq1z5pppa73ruz';
+
 module.exports = function(app) {
-  app.get('/', function(req, res) {
-   res.render('index');
-  });
+    app.get('/', function(req, res) {
+        api.streams.live({ game: 'Brawlhalla' }, (err, result) => {
+            if (err) console.log(err);
+            let top_streams = [];
+            for(let i = 0; i < 6; i++){
+                let stream = result.streams[i];
+                top_streams.push({
+                   stream_name: stream.channel.display_name,
+                   stream_status: stream.channel.status,
+                   stream_logo: stream.channel.logo,
+                   stream_viewers: stream.viewers,
+                   stream_preview_url: stream.preview.large,
+                   stream_url: stream.channel.url
+                });
+            }
+            res.render('index', { top_streams: top_streams });
+        });
+    });
 }
+
+api.streams.live
